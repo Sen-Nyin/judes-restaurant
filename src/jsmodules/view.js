7649dd733466c8754b927e8e95a9780c83d447e6
Leon Lonsdale <leonlonsdale@icloud.com>
2022-09-18 00:40:16 +0100

    nav clean up, accessibility, moved static elements to html

--- src/jsmodules/view.js
+++ src/jsmodules/view.js
@@ -18,9 +18,6 @@ export default class View extends Util {
   clearPage() {
     while (document.body.firstChild) document.body.firstChild.remove();
   }
-
-  // // header builders
-  // buildHead() {}
   buildHero() {
     const main = document.querySelector('main');
     const section = super.create(
@@ -37,7 +34,13 @@ export default class View extends Util {
       'text-slate-50',
       'text-center'
     );
-    const heroHeader = super.create('h1', 'text-7xl', 'mb-6');
+    const heroHeader = super.create(
+      'h1',
+      'text-5xl',
+      'mb-6',
+      'md:text-6xl',
+      'lg:text-7xl'
+    );
     heroHeader.textContent = 'We love food. We think you will too.';
     const subtext = super.create('h2', 'text-lg');
     subtext.textContent = "Fall in love with food again at Jude's";
@@ -46,45 +49,17 @@ export default class View extends Util {
     main.append(section);
   }
   buildNav() {
-    const header = document.querySelector('header');
-    // temp navlist
+    // TODO TEMP VAR
     const navItems = ['home', 'menu', 'contact'];
     //
-    // container
-    const container = super.create(
-      'div',
-      'wrapped',
-      'flex',
-      'flex-wrap',
-      'justify-between',
-      'items-center',
-      'text-slate-50'
-    );
+    const brandName = document.querySelector('[data-label="Brand Name"]');
+    const brandLocation = document.querySelector('[data-label="City"]');
+    const navList = document.querySelector('[data-label="Menu"]');
+    const navToggle = document.getElementById('navtoggle');
+    navToggle.addEventListener('click', this.toggleNav);
 
-    //branding
-    const brandBox = super.create('div', 'flex', 'flex-col', 'flex-1', 'py-2');
-    const brandText = super.create('span', 'font-bold', 'text-3xl');
-    brandText.textContent = "Jude's"; // TODO
-    const brandLoc = super.create('span', 'font-light', 'text-md');
-    brandLoc.textContent = 'Liverpool'; // TODO
-    brandBox.append(brandText, brandLoc);
-    // burger
-    const burger = super.create('button', 'block', 'md:hidden');
-    burger.id = 'navtoggle';
-    burger.textContent = 'Menu';
-    // navbar
-    const navbar = super.create('nav', 'w-full', 'md:w-auto');
-    const navList = super.create(
-      'ul',
-      'w-full',
-      'md:flex',
-      'md:gap-10',
-      'justify-between',
-      'w-full',
-      'hidden',
-      'md:block'
-    );
-    navList.id = 'navList';
+    brandName.textContent = "Jude's";
+    brandLocation.textContent = 'Liverpool';
     navItems.forEach((item) => {
       const listItem = super.create('li', 'w-full', 'text-center');
       const link = super.create('a', 'nav-link', 'md:py-6');
@@ -92,15 +67,15 @@ export default class View extends Util {
       listItem.append(link);
       navList.append(listItem);
     });
-    navbar.append(navList);
-    burger.addEventListener('click', this.toggleNav);
-    container.append(brandBox, burger, navbar);
-    header.append(container);
   }
   toggleNav() {
-    const navButton = document.getElementById('navToggle');
-    const navList = document.getElementById('navList');
+    const navButton = document.getElementById('navtoggle');
+    const navList = document.querySelector('[data-label="Menu"]');
+    navList.hasAttribute('data-visible')
+      ? navButton.setAttribute('aria-expanded', false)
+      : navButton.setAttribute('aria-expanded', true);
     navList.classList.toggle('hidden');
+    navList.toggleAttribute('data-visible');
   }
 
   // // content builders
